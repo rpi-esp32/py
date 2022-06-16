290d8db893ebd42850df11e26486124c89f4d23f
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-16 23:07:55 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,3 +9,5 @@ const a = [];
 //self.addEventListener("install", function (e) {e.waitUntil(caches.open(c).then(function (cache) {return cache.addAll(as);}));});
 
 //self.addEventListener("fetch", function (event) {console.log(event.request.url); event.respondWith(caches.match(event.request).then(function (response) {return response || fetch(event.request);}));});
+
+self.addEventListener("install", function (e) {return;};);
