abbff64e6f4b48ee8719c0df165b6a9e7a272850
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-16 22:30:43 +0800

    Add files via upload

---
+++ servicewoorker.js
@@ -0,0 +1,23 @@
+
+const staticCacheName = "pwa";
+
+const assets = ["/", "/index.html", "https://pyscript.net/alpha/pyscript.css", "https://pyscript.net/alpha/pyscript.js", "src/main.py", "src/main.js"]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            cache.addAll(assets).then(r => {
+                console.log("Cache assets downloaded");
+            }).catch(err => console.log("Error caching item", err))
+            console.log(`Cache ${staticCacheName} opened.`);
+        }).catch(err => console.log("Error opening cache", err))
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        }).catch(err => console.log("Cache fetch error: ", err))
+    )
+})
