a6ab6ce7bf34a06096ccb52a646ddc9d2c780bd5
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-16 23:19:26 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,38 @@
-const c = "pwa";
 
-//const assets = ["/", "/index.html", "https://pyscript.net/alpha/pyscript.css", "https://pyscript.net/alpha/pyscript.js", "src/main.py", "src/main.js"]
+//importScripts('serviceworker-cache-polyfill.js');
 
-//const a = ["/", "/index.html", "src/main.js", "src/main.py"]
 
-const a = [];
+self.addEventListener('install', function(event) {
+  // We pass a promise to event.waitUntil to signal how 
+  // long install takes, and if it failed
+  event.waitUntil(
+    // We open a cache…
+    caches.open('simple-sw-v1').then(function(cache) {
+      // And add resources to it
+      return cache.addAll([
+        './',
+        'style.css',
+        'logging.js'
+      ]);
+    })
+  );
+});
 
-//self.addEventListener("install", function (e) {e.waitUntil(caches.open(c).then(function (cache) {return cache.addAll(as);}));});
-
-//self.addEventListener("fetch", function (event) {console.log(event.request.url); event.respondWith(caches.match(event.request).then(function (response) {return response || fetch(event.request);}));});
-
-self.addEventListener("install", function (e) {return;};);
+// The fetch event happens for the page request with the
+// ServiceWorker's scope, and any request made within that
+// page
+self.addEventListener('fetch', function(event) {
+  // Calling event.respondWith means we're in charge
+  // of providing the response. We pass in a promise
+  // that resolves with a response object
+  event.respondWith(
+    // First we look for something in the caches that
+    // matches the request
+    caches.match(event.request).then(function(response) {
+      // If we get something, we return it, otherwise
+      // it's null, and we'll pass the request to
+      // fetch, which will use the network.
+      return response || fetch(event.request);
+    })
+  );
+});
