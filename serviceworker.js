483673509647c533cc80c685b9158a5957d4f79f
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-16 22:09:34 +0800

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,22 +1,9 @@
-var staticCacheName = "pwa";
+const staticCacheName = "pwa";
 
-const assets = [
-"/",
-"/index.html", 
-"https://pyscript.net/alpha/pyscript.css", 
-"https://pyscript.net/alpha/pyscript.js",
-"src/main.py",
-"src/main.js"
-]
+const assets = ["/", "/index.html", "https://pyscript.net/alpha/pyscript.css", "https://pyscript.net/alpha/pyscript.js", "src/main.py", "src/main.js"]
 
 
-self.addEventListener("install", function (e) {
-e.waitUntil(
-	caches.open(staticCacheName).then(function (cache) {
-	return cache.addAll(assets);
-	})
-);
-});
+self.addEventListener("install", function (e) {e.waitUntil(caches.open(staticCacheName).then(function (cache) {return cache.addAll(assets);}));});
 
 self.addEventListener("fetch", function (event) {
 console.log(event.request.url);
