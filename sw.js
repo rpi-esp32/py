c8e96a5df6bc29f41380ff0f0a2314555678ed4d
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-16 23:00:19 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,8 +2,10 @@ const c = "pwa";
 
 //const assets = ["/", "/index.html", "https://pyscript.net/alpha/pyscript.css", "https://pyscript.net/alpha/pyscript.js", "src/main.py", "src/main.js"]
 
-const a = ["/", "/index.html", "src/main.js", "src/main.py"]
+//const a = ["/", "/index.html", "src/main.js", "src/main.py"]
 
-self.addEventListener("install", function (e) {e.waitUntil(caches.open(c).then(function (cache) {return cache.addAll(as);}));});
+const a = [];
+
+//self.addEventListener("install", function (e) {e.waitUntil(caches.open(c).then(function (cache) {return cache.addAll(as);}));});
 
 //self.addEventListener("fetch", function (event) {console.log(event.request.url); event.respondWith(caches.match(event.request).then(function (response) {return response || fetch(event.request);}));});
