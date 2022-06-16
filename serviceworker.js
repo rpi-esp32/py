750aa780333a4a609a8f3e7dc79a3c3c57c6abb0
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-16 22:18:00 +0800

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,7 @@
 const staticCacheName = "pwa";
 
-const assets = ["/", "/index.html", "https://pyscript.net/alpha/pyscript.css", "https://pyscript.net/alpha/pyscript.js", "src/main.py", "src/main.js"]
-
+#const assets = ["/", "/index.html", "https://pyscript.net/alpha/pyscript.css", "https://pyscript.net/alpha/pyscript.js", "src/main.py", "src/main.js"]
+const assets = ["/", "/index.html", "src/main.py"]
 
 self.addEventListener("install", function (e) {e.waitUntil(caches.open(staticCacheName).then(function (cache) {return cache.addAll(assets);}));});
 
