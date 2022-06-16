1210f78d4b9b36fc2c0fa93012eb6c7680019989
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-16 15:33:17 +0800

    Update 3.h

--- 3.h
+++ 3.h
@@ -12,25 +12,9 @@
 </head>
 <body>
   <span></span>
-  <script>
-    function add(callback) {
-      return 1, 2, 3
-    }
+  <script src="src/main.js">
   </script>
-  <py-script>
-from js import add
-from pyodide import create_proxy
-import numpy as np
-
-span = document.querySelector("span")
-
-def callback(x, y, z):
-    span.innerText = orientation(x, y, z)
-
-def orientation(x, y, z):
-    return f"{x=}, {y=}, {z=}"
-
-add(create_proxy(callback))
+  <py-script src="src/main.py">
   </py-script>
 </body>
 </html>
