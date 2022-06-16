462cf76f1782bc667af5a226e5d89969f2f95028
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-17 01:50:11 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,6 +14,10 @@ self.addEventListener("install", installEvent => {
 //               .then(r => {}).catch(err => console.log("Error caching item", err))
 //        }).catch(err => console.log("Error opening cache", err))
 //    );
+    
+    installEvent.awaitUntil(caches.open(staticCacheName).then(cache => {cache.addAll(assests);}))
+    .then(r=>{console.log("cache installed by Maurice")});
+    
     console.log("finished maurice cache");
 });
 
