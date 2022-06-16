e502643ff6b46dd95cad01a97c85df467793b0b5
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-16 22:21:17 +0800

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,16 +1,19 @@
 const staticCacheName = "pwa";
 
-#const assets = ["/", "/index.html", "https://pyscript.net/alpha/pyscript.css", "https://pyscript.net/alpha/pyscript.js", "src/main.py", "src/main.js"]
+// const assets = ["/", "/index.html", "https://pyscript.net/alpha/pyscript.css", "https://pyscript.net/alpha/pyscript.js", "src/main.py", "src/main.js"]
 const assets = ["/", "/index.html", "src/main.py"]
 
-self.addEventListener("install", function (e) {e.waitUntil(caches.open(staticCacheName).then(function (cache) {return cache.addAll(assets);}));});
+//self.addEventListener("install", function (e) {e.waitUntil(caches.open(staticCacheName).then(function (cache) {return cache.addAll(assets);}));});
 
-self.addEventListener("fetch", function (event) {
-console.log(event.request.url);
+self.addEventListener("install", function (e) {});
+self.addEventListener("fetch", function (event) {});
 
-event.respondWith(
-	caches.match(event.request).then(function (response) {
-	return response || fetch(event.request);
-	})
-);
-});
+//self.addEventListener("fetch", function (event) {
+//console.log(event.request.url);
+//
+//event.respondWith(
+//	caches.match(event.request).then(function (response) {
+//	return response || fetch(event.request);
+//	})
+//);
+//});
