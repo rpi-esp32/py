81e9fbb0b7dfc7b96eb68f78d9456af5192e6d0b
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-17 08:24:03 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,8 +11,11 @@ console.log("after maurice assets printed");
 
 self.addEventListener("install", installEvent => {
     console.log("installing maurice cache");
-    installEvent.waitUntil(caches.open(staticCacheName))
-    .then((cache) => {cache.addAll(["/", "/index.html"]);console.log("cache.addALL success");}).catch(err => console.log("Error caching item", err));
+    installEvent.waitUntil(caches.open(staticCacheName)
+    .then((cache) => {cache.addAll(["/","/index.html"]);console.log("INSTALL cache.addALL success");}).catch(err => console.log("Error INSTALL caching item", err)));
+    console.log("INSTALL maurice success");
+//    installEvent.waitUntil(caches.open(staticCacheName))
+//    .then((cache) => {cache.addAll(["/", "/index.html"]);console.log("cache.addALL success");}).catch(err => console.log("Error caching item", err));
 //    then(cache => {cache.addAll(assets);}));
 //               .then(r => {}).catch(err => console.log("Error caching item", err))
 //        }).catch(err => console.log("Error opening cache", err))
